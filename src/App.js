0c55b962e96b3f13a6c9b61fcd7b7e1917836902
Nnebuife <95724536+Nnebuife@users.noreply.github.com>
2023-01-14 10:49:27 +0100

    App's GitHub URL added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+     <h1>  Weather App </h1>
+
+<footer>
+      <p> This project is done by Buife and is an {" "}  
+        <a href="https://github.com/Nnebuife/react-weather-app" target="_blank" rel="noreferrer"> 
+         open-source code
+        </a>
+       {" "}on GitHub.
+      </p>
+      </footer>
     </div>
   );
 }
