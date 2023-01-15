901c1b124715a6140beb166fddc43927f6028b87
Nnebuife <95724536+Nnebuife@users.noreply.github.com>
2023-01-15 18:56:53 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
+import React from "react";
 import './App.css';
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
      <h1>  Weather App </h1>
-
+<Weather />
+<button class = "btn btn-success"> Welcome </button>
 <footer>
       <p> This project is done by Buife and is an {" "}  
-        <a href="https://github.com/Nnebuife/react-weather-app" target="_blank" rel="noreferrer"> 
+        <a href="https://github.com/Nnebuife/react-weather-app" target="_blank" rel="noopener noreferrer"> 
          open-source code
         </a>
        {" "}on GitHub.
       </p>
       </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+
+export default function Weather() {
+    return <div> It's a Weather app 
+            </div>
+           }
